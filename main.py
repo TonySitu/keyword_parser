fa85ab09a95663dab20ba3c8be356af06c877762
Tony <137113805+TonySitu@users.noreply.github.com>
2024-07-18 20:18:52 -0400

    added parse_file_name function

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import docx
+import argparse
+
+
+def parse_file_name(file_name):
+    parser = argparse.ArgumentParser(
+        prog='keyword Parser',
+        description="Finds and updates keywords wrapped with '[]' with user input")
+    parser.add_argument(file_name, help='Name of the file to be parsed')
+    file_name = parser.parse_args()
+
+    return file_name
+
+
+def main():
+    pass
+
 
 if __name__ == "__main__":
-    print('hello')
+    main()
